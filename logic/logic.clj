60391b54477c8a113051f1d249ed467727fdff98
Fernando Gómez <gomezhyuuga@gmail.com>
2016-04-27 17:55:44 -0500

    Logic programming

---
+++ logic/logic.clj
@@ -0,0 +1,22 @@
+(use 'clojure.core.logic)
+
+(defn my-last
+  [lst]
+  (cond
+    (empty? (rest lst)) (first lst)
+    (not (empty? (rest lst))) (my-last (rest lst))))
+
+(defn lasto "logic last" [lst result]
+  (fresh [h t]
+         (conde
+           [(conso result t lst) (== t [])]
+           [(conso h t lst)      (!= t []) (lasto t result)])))
+
+(defn reverseo "reverse a list" [lst result]
+  (fresh [h t x]
+         (conde
+           [(== lst []) (== result [])]
+           [(!= lst [])
+            (conso h t lst)
+            (reverseo t x)
+            (appendo x [h] result)])))
